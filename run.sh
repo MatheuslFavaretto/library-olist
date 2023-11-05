55f9e6dc0813c8ebf67eb55b33c39e461cb4eb8a
Matheus Favaretto <116848225+MatheuslFavaretto@users.noreply.github.com>
2023-11-05 13:33:07 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 python manage.py test
+
 python manage.py makemigrations
 python manage.py migrate
 
-
 python manage.py import_authors authors.csv
 
-python manage.py runserver 0.0.0.0:8000
+nohup python manage.py runserver 0.0.0.0:8000 &
+
+tail -f /dev/null
